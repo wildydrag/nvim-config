5fd81e97e682561a152f24a243f1d2f72f3508ee
emad arch linux pc <emad.33mohammad35@gmail.com>
2025-05-23 16:05:34 +0330

    add git.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,6 +9,9 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" }
 vim.keymap.set("n", "<leader>f", ":NvimTreeFocus<CR>" , {desc = "Focus File Tree"})
 vim.keymap.set("n", "<leader>n", ":NvimTreeFindFile<CR>", {desc = "Find Current File in Tree"})
 vim.keymap.set("i", "<C-H>", "<C-W>", { desc = "Delete word backward" })
+vim.keymap.set('n', '<leader>do', ':DiffviewOpen<CR>', { desc = "Open Diffview" })
+vim.keymap.set('n', '<leader>dc', ':DiffviewClose<CR>', { desc = "Close Diffview" })
+vim.keymap.set('n', '<leader>dh', ':DiffviewFileHistory<CR>', { desc = "File History" })
 
 
 local themes = { "tokyonight", "catppuccin", "gruvbox", "rose-pine", "nord", "nordic" }
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,26 @@
 return {
-    {"lewis6991/gitsigns.nvim"},
-    {"tpope/vim-fugitive"},
-    {"sindrets/diffview.nvim"}
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup {
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local function map(mode, lhs, rhs, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, lhs, rhs, opts)
+          end
+
+          -- Keybindings
+          map('n', ']c', gs.next_hunk)
+          map('n', '[c', gs.prev_hunk)
+          map({'n', 'v'}, '<leader>hs', gs.stage_hunk)
+          map({'n', 'v'}, '<leader>hr', gs.reset_hunk)
+          map('n', '<leader>hp', gs.preview_hunk)
+        end,
+      }
+    end
+  },
+  { "tpope/vim-fugitive" },
+  { "sindrets/diffview.nvim" },
 }
