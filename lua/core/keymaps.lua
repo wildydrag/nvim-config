1818e924ee811a32dbdfe0423480cadf5911835c
emad arch linux pc <emad.33mohammad35@gmail.com>
2025-05-23 16:15:34 +0330

    add double Esc keybinding to save the changes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,6 +9,7 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" }
 vim.keymap.set("n", "<leader>f", ":NvimTreeFocus<CR>" , {desc = "Focus File Tree"})
 vim.keymap.set("n", "<leader>n", ":NvimTreeFindFile<CR>", {desc = "Find Current File in Tree"})
 vim.keymap.set("i", "<C-H>", "<C-W>", { desc = "Delete word backward" })
+vim.keymap.set({'n', 'i', 'v', 'c'}, '<Esc><Esc>', '<Esc>:w<CR>', { noremap = true })
 -- Keymaps for system clipboard
 vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', {desc = "Yank to system clipboard"})
 vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', {desc = "Paste from system clipboard"})
