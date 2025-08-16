c7280453d12e5dbb2ca81d465bebf81f08d71bbe
emad-changizi <emad.33mohammad35@gmail.com>
2025-08-16 18:07:46 +0330

    add a keymap for todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,9 +1,11 @@
 return {
   "folke/todo-comments.nvim",
+    lazy=false,
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
     -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+  },
+  keys = {
+    { "<leader>fT", "<Cmd>TodoTelescope<CR>", desc = "open all todos, warnings, etc." },
+  },
 }
